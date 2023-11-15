d2cad63ee04e04c330a438404ca117f91eb57cc8
gkrotkov <gabriel.gk.mail@gmail.com>
2023-11-14 22:56:38 -0500

    load-hhs beginning

--- load-hhs.py
+++ load-hhs.py
@@ -1,4 +1,11 @@
 import psycopg
+import pandas as pd
+import sys
+
+filename = sys.argv[1]
+print("!!!!!!!!!!!!!!!!!!!!!!")
+print(filename)
+batch = pd.read_csv(filename)
 
 conn = psycopg.connect(
     host="pinniped.postgres.database.azure.com", dbname="gkrotkov",
