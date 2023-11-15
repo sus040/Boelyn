a0d53738e9f7a561e016a6e1234c0fdc3c2e5e48
gkrotkov <gabriel.gk.mail@gmail.com>
2023-11-15 09:01:37 -0500

    adding credentials, load-hhs inserts

---
+++ credentials_template.py
@@ -0,0 +1,5 @@
+# credentials template
+
+DBNAME = "put_your_dbname_here"
+USER = "put_your_user_here"
+PASSWORD = "put_your_password_here"
--- load-hhs.py
+++ load-hhs.py
@@ -1,13 +1,45 @@
 import psycopg
 import pandas as pd
 import sys
+from credentials import DBNAME, USER, PASSWORD
 
 filename = sys.argv[1]
-print("!!!!!!!!!!!!!!!!!!!!!!")
-print(filename)
 batch = pd.read_csv(filename)
 
+# Data Cleaning
+
+
+# This is the weekly beds information for each hospital, needs to be inserted
+# into the `beds` table.
+
 conn = psycopg.connect(
-    host="pinniped.postgres.database.azure.com", dbname="gkrotkov",
-    user="gkrotkov", password="5uqFbI&rRi"
+    host="pinniped.postgres.database.azure.com",
+    dbname=DBNAME, user=USER, password=PASSWORD
 )
+
+cur = conn.cursor()
+
+for row in batch:
+    cur.execute("INSERT INTO beds (hospital_pk, collection_week, "
+                "all_adult_hospital_beds_7_day_avg, "
+                "all_pediatric_inpatient_beds_7_day_avg, "
+                "all_adult_hospital_inpatient_bed_occupied_7_day_coverage, "
+                "all_pediatric_inpatient_bed_occupied_7_day_avg, "
+                "total_icu_beds_7_day_avg, icu_beds_used_7_day_avg, "
+                "inpatient_beds_used_covid_7_day_avg, "
+                "staffed_icu_adult_patients_confirmed_covid_7_day_avg)"
+                "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                (str(row['hospital_pk']), str(row['collection_week']),
+                 str(row['all_adult_hospital_beds_7_day_avg']),
+                 str(row['all_pediatric_inpatient_beds_7_day_avg']),
+                 str(row['all_adult_hospital_inpatient_bed_occupied_7_'
+                         'day_coverage']),
+                 str(row['all_pediatric_inpatient_bed_occupied_7_day_avg']),
+                 str(row['total_icu_beds_7_day_avg']),
+                 str(row['icu_beds_used_7_day_avg']),
+                 str(row['inpatient_beds_used_covid_7_day_avg']),
+                 str(row['staffed_icu_adult_patients_confirmed'
+                         '_covid_7_day_avg'])))
+
+conn.commit()
+conn.close()
