73a319dd1514599635b110a5ee901c8cc2c6bd5f
gkrotkov <gabriel.gk.mail@gmail.com>
2023-11-15 12:27:35 -0500

    changing hhs data types to real

--- load-hhs.py
+++ load-hhs.py
@@ -33,17 +33,16 @@ for idx, row in batch.iterrows():
                 "inpatient_beds_used_covid_7_day_avg, "
                 "staffed_icu_adult_patients_confirmed_covid_7_day_avg)"
                 "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
-                (str(row['hospital_pk']), str(row['collection_week']),
-                 str(row['all_adult_hospital_beds_7_day_avg']),
-                 str(row['all_pediatric_inpatient_beds_7_day_avg']),
-                 str(row['all_adult_hospital_inpatient_bed_occupied_7_'
-                         'day_coverage']),
-                 str(row['all_pediatric_inpatient_bed_occupied_7_day_avg']),
-                 str(row['total_icu_beds_7_day_avg']),
-                 str(row['icu_beds_used_7_day_avg']),
-                 str(row['inpatient_beds_used_covid_7_day_avg']),
-                 str(row['staffed_icu_adult_patients_confirmed'
-                         '_covid_7_day_avg'])))
+                (row['hospital_pk'], row['collection_week'],
+                 row['all_adult_hospital_beds_7_day_avg'],
+                 row['all_pediatric_inpatient_beds_7_day_avg'],
+                 row['all_adult_hospital_inpatient_bed_occupied_7_'
+                     'day_coverage'],
+                 row['all_pediatric_inpatient_bed_occupied_7_day_avg'],
+                 row['total_icu_beds_7_day_avg'],
+                 row['icu_beds_used_7_day_avg'],
+                 row['inpatient_beds_used_covid_7_day_avg'],
+                 row['staffed_icu_adult_patients_confirmed_covid_7_day_avg']))
 
 conn.commit()
 conn.close()
--- make_tables.sql
+++ make_tables.sql
@@ -16,14 +16,14 @@ CREATE TABLE beds (
     record_id SERIAL PRIMARY KEY,
     hospital_pk VARCHAR(255) NOT NULL,
     collection_week DATE NOT NULL,
-    all_adult_hospital_beds_7_day_avg INT,
-    all_pediatric_inpatient_beds_7_day_avg INT,
-    all_adult_hospital_inpatient_bed_occupied_7_day_coverage INT,
-    all_pediatric_inpatient_bed_occupied_7_day_avg INT,
-    total_icu_beds_7_day_avg INT,
-    icu_beds_used_7_day_avg INT,
-    inpatient_beds_used_covid_7_day_avg INT,
-    staffed_icu_adult_patients_confirmed_covid_7_day_avg INT,
+    all_adult_hospital_beds_7_day_avg REAL,
+    all_pediatric_inpatient_beds_7_day_avg REAL,
+    all_adult_hospital_inpatient_bed_occupied_7_day_coverage REAL,
+    all_pediatric_inpatient_bed_occupied_7_day_avg REAL,
+    total_icu_beds_7_day_avg REAL,
+    icu_beds_used_7_day_avg REAL,
+    inpatient_beds_used_covid_7_day_avg REAL,
+    staffed_icu_adult_patients_confirmed_covid_7_day_avg REAL,
     FOREIGN KEY (hospital_pk) REFERENCES hospital(hospital_pk)
 );
 
