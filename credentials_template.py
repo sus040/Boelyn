be8352127d7e92448d08f7dffceb541c2b8d95a0
jjamiiekim4203 <jamiekim1126@gmail.com>
2023-11-16 12:50:11 -0500

    updated

--- credentials_template.py
+++
@@ -1,8 +0,0 @@
-# credentials template
-# to pass in credentials to the `load-hhs.py` script, copy this script, fill it
-# with your credentials, and ensure it is in the same working directory as
-# load-hhs.py
-
-DBNAME = "put_your_dbname_here"
-USER = "put_your_user_here"
-PASSWORD = "put_your_password_here"
--- load-quality.py
+++ load-quality.py
@@ -1,48 +1,66 @@
+import os
 import argparse
 import pandas as pd
-import psycopg
+import psycopg2
+from datetime import datetime
 
-parser = argparse.ArgumentParser(
-    description='Load hospital quality data into the database.')
-parser.add_argument(
-    'date', type=str, help='Date of the quality data (YYYY-MM-DD)')
+# Define command-line arguments
+parser = argparse.ArgumentParser(description=("Load hospital quality data"
+                                              "into the database."))
+parser.add_argument('date', type=str, help="Date of the quality data"
+                    "(YYYY-MM-DD)")
 parser.add_argument('csv_file', type=str, help='CSV file name')
 args = parser.parse_args()
 
-
+# Read data from CSV file into a Pandas DataFrame
 try:
     data = pd.read_csv(args.csv_file)
+    print("Data read from CSV successfully:")
+    print(data.head())  # Print the first few rows of the DataFrame
 except Exception as e:
     print(f"Error reading CSV file: {e}")
     exit()
 
 
-try:
-    conn = psycopg.connect(
-        host="pinniped.postgres.database.azure.com",
-        dbname="jamiekim",
-        user="jamiekim",
-        password="aBG1x?8P96"
-    )
-    cursor = conn.cursor()
-except Exception as e:
-    print(f"Error connecting to the database: {e}")
-    exit()
-
-
+# Define a function to insert data into the PostgreSQL table
 def insert_quality_data(quality_data, date):
-    for index, row in quality_data.iterrows():
-        cursor.execute("INSERT INTO quality (Facility_ID, hospital_type,\
-                       hospital_ownership, emergency_services, quality_rating,\
-                       rating_date) VALUES (%s, %s, %s, %s, %s, %s)",
-                       (row['facility_id'], row['hospital_type'],
-                        row['hospital_ownership'], row['emergency_services'],
-                        row['hospital_overall_rating'], date))
+    try:
+        print("Connecting to the database...")
+        with psycopg2.connect(
+            host=os.environ.get('DB_HOST'),
+            dbname=os.environ.get('DB_NAME'),
+            user=os.environ.get('DB_USER'),
+            password=os.environ.get('DB_PASSWORD')
+        ) as conn:
+
+            with conn.cursor() as cursor:
+                print("Connected. Checking if 'quality' table exists...")
+                cursor.execute("SELECT to_regclass('quality');")
+                result = cursor.fetchone()
+                if result and result[0]:
+                    print("Table 'quality' exists. Proceeding with data"
+                          "insertion.")
+                else:
+                    print("Table 'quality' does not exist. Exiting.")
+                    return
+
+                for _, row in quality_data.iterrows():
+                    cursor.execute(
+                        "INSERT INTO quality (Facility_ID, hospital_type, \
+                            hospital_ownership, emergency_services, \
+                                quality_rating, rating_date) VALUES (%s, %s, \
+                                    %s, %s, %s, %s)",
+                        (row['Facility ID'], row['Hospital Type'],
+                         row['Hospital Ownership'], row['Emergency Services'],
+                         row['Hospital overall rating'],
+                         datetime.strptime(date, '%Y-%m-%d')))
+
+                conn.commit()
+                print("Data successfully inserted into the 'quality' table")
+
+    except Exception as e:
+        print(f"Error inserting data into the database: {e}")
 
 
+# Call the function to insert data into the PostgreSQL table
 insert_quality_data(data, args.date)
-
-
-conn.commit()
-cursor.close()
-conn.close()
