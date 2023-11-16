8cf80266449da9586513d33fcc6ed6b6752ccb6a
gkrotkov <gabriel.gk.mail@gmail.com>
2023-11-16 13:46:18 -0500

    style

--- load-hhs.py
+++ load-hhs.py
@@ -22,28 +22,47 @@ conn = psycopg.connect(
 )
 
 cur = conn.cursor()
+successes, fails = 0, 0
+
+# ASSUMPTION: the hospital_pk column is unique, so we have 1 hospital per row
+for idx, row in batch.iterrows():
+    try:
+        cur.execute()
+        successes += 1
+    except Exception:  # should make this specific
+        fails += 1
+
+print("Successfully added:", str(successes), "rows to the hospitals table."
+      "\n" + str(fails) + "rows rejected", sep=" ")
+successes, fails = 0, 0
 
 for idx, row in batch.iterrows():
-    # check if this hospital is in the database already
-    cur.execute("INSERT INTO beds (hospital_pk, collection_week, "
-                "all_adult_hospital_beds_7_day_avg, "
-                "all_pediatric_inpatient_beds_7_day_avg, "
-                "all_adult_hospital_inpatient_bed_occupied_7_day_coverage, "
-                "all_pediatric_inpatient_bed_occupied_7_day_avg, "
-                "total_icu_beds_7_day_avg, icu_beds_used_7_day_avg, "
-                "inpatient_beds_used_covid_7_day_avg, "
-                "staffed_icu_adult_patients_confirmed_covid_7_day_avg)"
-                "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
-                (row['hospital_pk'], row['collection_week'],
-                 row['all_adult_hospital_beds_7_day_avg'],
-                 row['all_pediatric_inpatient_beds_7_day_avg'],
-                 row['all_adult_hospital_inpatient_bed_occupied_7_'
-                     'day_coverage'],
-                 row['all_pediatric_inpatient_bed_occupied_7_day_avg'],
-                 row['total_icu_beds_7_day_avg'],
-                 row['icu_beds_used_7_day_avg'],
-                 row['inpatient_beds_used_covid_7_day_avg'],
-                 row['staffed_icu_adult_patients_confirmed_covid_7_day_avg']))
+    try:
+        cur.execute("INSERT INTO beds (hospital_pk, collection_week, "
+                    "all_adult_hospital_beds_7_day_avg, "
+                    "all_pediatric_inpatient_beds_7_day_avg, "
+                    "all_adult_hospital_inpatient_bed_"
+                    "occupied_7_day_coverage, "
+                    "all_pediatric_inpatient_bed_occupied_7_day_avg, "
+                    "total_icu_beds_7_day_avg, icu_beds_used_7_day_avg, "
+                    "inpatient_beds_used_covid_7_day_avg, "
+                    "staffed_icu_adult_patients_confirmed_covid_7_day_avg)"
+                    "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                    (row['hospital_pk'], row['collection_week'],
+                     row['all_adult_hospital_beds_7_day_avg'],
+                     row['all_pediatric_inpatient_beds_7_day_avg'],
+                     row['all_adult_hospital_inpatient_bed_occupied_7_'
+                         'day_coverage'],
+                     row['all_pediatric_inpatient_bed_occupied_7_day_avg'],
+                     row['total_icu_beds_7_day_avg'],
+                     row['icu_beds_used_7_day_avg'],
+                     row['inpatient_beds_used_covid_7_day_avg'],
+                     row[
+                         'staffed_icu_adult_patients_confirmed_covid_7_day_avg'
+                        ]))
+        successes += 1
+    except Exception:  # should make this specific
+        fails += 1
 
 conn.commit()
 conn.close()
