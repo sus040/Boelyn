a3d9fb358e45e393dfc2d7ae233c0c48897cb71a
jjamiiekim4203 <jamiekim1126@gmail.com>
2023-11-16 12:56:36 -0500

    removing comment in template

--- credentials_template.py
+++ credentials_template.py
@@ -2,7 +2,6 @@
 # to pass in credentials to the `load-hhs.py` script, copy this script, fill it
 # with your credentials, and ensure it is in the same working directory as
 # load-hhs.py
-# adding a comment to the template
 
 DBNAME = "put_your_dbname_here"
 USER = "put_your_user_here"
