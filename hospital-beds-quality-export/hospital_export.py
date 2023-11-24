fa9f32cb1083f80c3ae9b5e7431fecb44b5b6c4e
jjamiiekim4203 <jamiekim1126@gmail.com>
2023-11-23 22:56:36 -0500

    exporting scripts

---
+++ hospital-beds-quality-export/hospital_export.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import psycopg
+from credentials import DBNAME, USER, PASSWORD
+
+conn = psycopg.connect(
+    host="pinniped.postgres.database.azure.com",
+    dbname=DBNAME, user=USER, password=PASSWORD
+)
+
+query = "SELECT * FROM hospital;"
+
+df = pd.read_sql(query, conn)
+
+conn.close()
+
+df.to_csv('~/Desktop/hospital.csv', index=False)
