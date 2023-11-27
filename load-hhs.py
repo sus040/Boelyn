9dea36d9f0168a57d4589635ab005e2718352bee
gkrotkov <gabriel.gk.mail@gmail.com>
2023-11-27 15:10:14 -0500

    adding negative checks to table declaration

--- load-hhs.py
+++ load-hhs.py
@@ -28,8 +28,7 @@ print("Successfully read:", len(batch), "rows from file.")
 # Data Cleaning
 replacements = {'-999999.0': None, '-9999': None, -999999.0: None, -9999: None,
                 'NA': None, np.nan: None}
-batch.replace(to_replace=replacements,
-              inplace=True)
+batch.replace(to_replace=replacements, inplace=True)
 batch['collection_week'] = pd.to_datetime(batch['collection_week'])
 
 # Duplicate check
