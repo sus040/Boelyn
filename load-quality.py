6ce864029b0766822b7cadaf332672c8de7cc598
suyeons <suyeons@andrew.cmu.edu>
2023-11-29 01:26:48 -0500

    skipped rows file and operation time added

--- load-quality.py
+++ load-quality.py
@@ -1,79 +1,116 @@
-import argparse
-import pandas as pd
-from datetime import datetime
-import psycopg
-from credentials import DBNAME, USER, PASSWORD  # check credentials_template.py
+import pandas as pd  # Import pandas library for data manipulation
+import numpy as np  # Import numpy library for numerical operations
+from datetime import datetime  # Import datetime for handling dates
+import psycopg  # Import psycopg for postgreSQL database connection
+from credentials import DBNAME, USER, PASSWORD  # Import database credentials
+import argparse  # Import argparse for parsing command-line arguments
+import time  # Import time for tracking execution time
 
-# Define command-line arguments
+# Set up command-line argument parsing
 parser = argparse.ArgumentParser(
-    description='Load hospital quality data into the database.')
-parser.add_argument(
-    'date', type=str, help='Date of the quality data (YYYY-MM-DD)')
-parser.add_argument('csv_file', type=str, help='CSV file name')
-args = parser.parse_args()
-
-# Read data from CSV file into a pandas dataframe
-try:
-    data = pd.read_csv(args.csv_file)
-    print("Data read from CSV successfully:")
-    print(data.head())  # Print the first few rows of the dataframe
-except Exception as e:
-    print(f"Error reading CSV file: {e}")
-    exit()
-
-# Data Cleaning
-data['Hospital overall rating'].replace('Not Available', None, inplace=True)
-
-# Define a function to insert data into the postgreSQL table
+    description='Load hospital quality data into the database.'
+)
+parser.add_argument('date', help='Date of the quality data (YYYY-MM-DD)')
+parser.add_argument('csv_file', help='CSV file name')
+args = parser.parse_args()  # Parse the arguments provided by the user
 
 
-def insert_quality_data(quality_data, date):
+def connect_to_database():
+    """Establishes and returns a database connection."""
     try:
-        print("Connecting to the database...")
-        with psycopg.connect(
+        # Connect to the postgreSQL database
+        conn = psycopg.connect(
             host="pinniped.postgres.database.azure.com",
             dbname=DBNAME, user=USER, password=PASSWORD
-        ) as conn:
-
-            with conn.cursor() as cursor:
-                print("Connected. Checking if 'quality' table exists.")
-                cursor.execute("SELECT to_regclass('quality');")
-                result = cursor.fetchone()
-                if result and result[0]:
-                    print("Table 'quality' exists. Proceeding.")
-                else:
-                    print("Table 'quality' does not exist. Exiting.")
-                    return
-
-                skipped_rows = 0
-                for _, row in quality_data.iterrows():
-                    cursor.execute("SELECT COUNT(*) FROM hospital WHERE "
-                                   "hospital_pk = %s", (row['Facility ID'],))
-                    if cursor.fetchone()[0] == 0:
-                        skipped_rows += 1
-                        print(f"Facility ID {row['Facility ID']} not found in "
-                              "hospital table. Skipping.")
-                        continue
-                    cursor.execute(
-                        "INSERT INTO quality (Facility_ID, "
-                        "hospital_type, "
-                        "hospital_ownership, emergency_services, "
-                        "quality_rating, rating_date)"
-                        "VALUES (%s, %s, %s, %s, %s, %s)",
-                        (row['Facility ID'],
-                         row['Hospital Type'],
-                         row['Hospital Ownership'],
-                         row['Emergency Services'],
-                         row['Hospital overall rating'],
-                         datetime.strptime(date, '%Y-%m-%d')))
-
-                conn.commit()
-                print("Data successfully inserted into the 'quality' table.")
-                print(f"Number of skipped rows: {skipped_rows}")
-
+        )
+        print("Connected to the database.")
+        return conn  # Return the connection object
     except Exception as e:
-        print(f"Error inserting data into the database: {e}")
+        print(f"Error connecting to the database: {e}")
+        exit()  # Exit the script in case of connection failure
 
 
-# Call the function to insert data into the postgreSQL table
-insert_quality_data(data, args.date)
+def read_and_clean_data(csv_file):
+    """Reads and cleans data from a CSV file."""
+    try:
+        data = pd.read_csv(csv_file)  # Read data from the CSV file
+        print("Data read from CSV successfully:")
+        print(data.head())  # Display the first few rows of the dataframe
+        # Replace 'Not Available' with None (null) and NaN with None
+        data['Hospital overall rating'].replace(
+            'Not Available', None, inplace=True)
+        data.replace(np.nan, None, inplace=True)
+        return data  # Return the cleaned data
+    except Exception as e:
+        print(f"Error reading CSV file: {e}")
+        exit()  # Exit the script in case of file read failure
+
+
+def insert_quality_data(conn, quality_data, date):
+    """Inserts data into the postgreSQL table."""
+    start_time = time.time()  # Record the start time of the operation
+    # Convert Facility ID to string
+    quality_data['Facility ID'] = quality_data['Facility ID'].astype(str)
+
+    with conn.cursor() as cursor:  # Open a cursor
+        # Check if the 'quality' table exists
+        cursor.execute("SELECT to_regclass('quality');")
+        if cursor.fetchone():
+            print("Table 'quality' exists. Proceeding.")
+        else:
+            print("Table 'quality' does not exist. Exiting.")
+            return
+
+        # Fetch existing hospital primary keys
+        cursor.execute("SELECT hospital_pk FROM hospital")
+        existing_hospital_pks = set(row[0] for row in cursor.fetchall())
+
+        insert_data = []  # List to store data for bulk insertion
+        skipped_rows_data = []  # List to store skipped rows
+        for _, row in quality_data.iterrows():  # Iterate over each row
+            if row['Facility ID'] not in existing_hospital_pks:
+                # Add to skipped rows if not found
+                skipped_rows_data.append(row)
+                continue  # Skip to the next row
+
+            # Prepare the row for insertion
+            insert_data.append((
+                row['Facility ID'],
+                row['Hospital Type'],
+                row['Hospital Ownership'],
+                row['Emergency Services'],
+                row['Hospital overall rating'],
+                datetime.strptime(date, '%Y-%m-%d')
+            ))
+
+        # Execute bulk insert into the 'quality' table
+        cursor.executemany(
+            "INSERT INTO quality (Facility_ID, "
+            "hospital_type, hospital_ownership, "
+            "emergency_services, quality_rating, rating_date) "
+            "VALUES (%s, %s, %s, %s, %s, %s)",
+            insert_data
+        )
+        conn.commit()  # Commit the transaction
+        print("Data successfully inserted into the 'quality' table.")
+
+        # Display and save the number of skipped rows
+        num_skipped_rows = len(skipped_rows_data)
+        print(f"Number of skipped rows: {num_skipped_rows}")
+        if skipped_rows_data:
+            skipped_df = pd.DataFrame(skipped_rows_data)
+            skipped_df.to_csv(f"skipped_rows_{date}.csv", index=False)
+            print(f"Skipped rows saved to skipped_rows_{date}.csv")
+
+    end_time = time.time()  # Record the end time of the operation
+    elapsed_time = end_time - start_time  # Calculate the total elapsed time
+    minutes = int(elapsed_time // 60)  # Convert time to minutes
+    seconds = int(elapsed_time % 60)  # Convert time to seconds
+    print(f"Operation completed in {minutes} minutes and {seconds} seconds.")
+
+
+# Main execution process
+data = read_and_clean_data(args.csv_file)  # Read and clean the CSV data
+conn = connect_to_database()  # Establish a database connection
+insert_quality_data(conn, data, args.date)  # Insert data into the database
+conn.close()  # Close the database connection
