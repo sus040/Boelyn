57961f486db7dd8bd417262cf06e470157073f3b
gkrotkov <gabriel.gk.mail@gmail.com>
2023-12-04 07:48:46 -0500

    cleanup

--- load-hhs.py
+++ load-hhs.py
@@ -26,7 +26,6 @@ except FileNotFoundError:
 print("Successfully read:", len(batch), "rows from file.")
 
 # Data Cleaning
-# @TODO enforce nonnegativity here
 replacements = {'-999999.0': None, '-9999': None, -999999.0: None, -9999: None,
                 'NA': None, np.nan: None}
 batch.replace(to_replace=replacements, inplace=True)
