ed3ce43e42b19a12f620693a0aeeee61df1a9776
gkrotkov <gabriel.gk.mail@gmail.com>
2023-12-06 16:35:58 -0500

    changing error handling to parse out duplication

--- load-hhs.py
+++ load-hhs.py
@@ -4,7 +4,8 @@ import os
 import pandas as pd
 import numpy as np
 from credentials import DBNAME, USER, PASSWORD  # check credentials_template.py
-from helpers import hospital_insert, beds_insert, report_insert_results
+from helpers import hospital_insert, beds_insert, \
+    report_insert_results, filter_duplication_errors
 
 parser = argparse.ArgumentParser()
 parser.add_argument("filename", action="store")
@@ -86,12 +87,18 @@ cur = conn.cursor()
 #############
 
 # Insert hospital data, write errors to the errors folder
-report_insert_results(hospital_insert(conn, cur, batch), "hospital",
+results = hospital_insert(conn, cur, batch)
+results = filter_duplication_errors(results, "error/hospital_dup_cases.csv",
+                                    "error/hospital_dup_msgs.csv")
+report_insert_results(results, "hospital",
                       "error/hospital_errors.csv", "error/hospital_msgs.csv")
 conn.commit()
 
 # Insert beds data, write errors to the errors folder
-report_insert_results(beds_insert(conn, cur, batch), "beds",
+results = beds_insert(conn, cur, batch)
+results = filter_duplication_errors(results, "error/beds_dup_cases.csv",
+                                    "error/beds_dup_msgs.csv")
+report_insert_results(results, "beds",
                       "error/beds_errors.csv", "error/beds_msgs.csv")
 conn.commit()
 
